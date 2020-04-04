800459ec147af3577ecc0d1478c124dd9e7efd78
Frederick-S <mao_xiaodan@hotmail.com>
2020-04-04 17:40:19 +0800

    test: add tokenizer controller test

---
+++ src/test/java/jmo/TokenizerControllerTest.java
@@ -0,0 +1,36 @@
+package jmo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jmo.dto.AnalysisRequest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class TokenizerControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldGenerateTokens() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        AnalysisRequest analysisRequest = new AnalysisRequest();
+        analysisRequest.setText("もしも自分のことを「馬鹿ブス貧乏」と言われたなら、当然のことながらいい気はしないだろう。");
+
+        mockMvc.perform(post("/tokenize")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(analysisRequest)))
+                .andExpect(status().isOk())
+                .andDo(mvcResult -> {
+                    String body = mvcResult.getResponse().getContentAsString();
+
+                    Assertions.assertTrue(body.length() > 0);
+                });
+    }
+}
