9135381a16cd18ad7a06448e080e3b29336fab4e
Frederick-S <mao_xiaodan@hotmail.com>
2020-04-05 19:25:59 +0800

    config: add cross origin

--- src/main/java/jmo/controller/TokenizerController.java
+++ src/main/java/jmo/controller/TokenizerController.java
@@ -6,6 +6,7 @@ import jmo.dto.AnalysisRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class TokenizerController {
     @ResponseBody
